4945bc4f2791357a6524169c6dee3d77a4d3b7cc
nimboo-zrad <nimazr800@gmail.com>
2025-03-22 02:14:10 +0330

    modified package.json and get.js files

--- get.js
+++ get.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('info', (req, res)=>{
-    res.sendFile("hi this is get!");
+app.get('/flights', (req, res)=>{
+    res.send([
+        {
+        	destination: "moscow",
+            duration: 4
+        },
+        {
+        	destination: "dubai",
+            duration: 2
+        }
+    ]);
 });
 
 app.listen(PORT, ()=> console.log(`The server is running at port ${PORT}`));
